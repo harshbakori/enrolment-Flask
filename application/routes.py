3f5f9dbdbe0a2a09b8c6211f2e80c569f20c7621
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-06-08 22:19:31 +0530

    created models in models.py

--- application/routes.py
+++ application/routes.py
@@ -43,13 +43,7 @@ def api(idx=None):
     
     return Response(json.dumps(jdata),mimetype="application/json")
 
-class User(db.Document):
-    user_id     =   db.IntField(unique=True)
-    first_name  =   db.StringField(max_length=50)
-    last_name   =   db.StringField(max_length=50)
-    email       =   db.StringField(max_length=30)
-    password    =   db.StringField(max_length=30)
-    
+
     
 @app.route('/user')
 def user():
