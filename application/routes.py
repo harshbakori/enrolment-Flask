48f425c4a57f616315e85362a3ea0667a01ecaf0
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-06-08 10:54:36 +0530

    sent a json responce for api

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,8 @@
 from application import app
-from flask import render_template,request
+from flask import render_template,request,Response,json
+
+
+courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
 @app.route("/")
 @app.route("/index")
@@ -15,7 +18,6 @@ def login():
 @app.route("/courses/<term>")
 
 def courses(term="Spring 2019"):
-    courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
     # print(courseData[0]["title"])
     return render_template("courses.html", courseData=courseData, courses = True,term=term )
 
@@ -31,3 +33,12 @@ def enrollment():
     
     return render_template("enrollment.html", enrollment=True,data={"id":id,"title":title,"term":term})
     
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if(idx==None):
+        jdata=courseData
+    else:
+        jdata=courseData[int(idx)]
+    
+    return Response(json.dumps(jdata),mimetype="application/json")
