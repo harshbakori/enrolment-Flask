035e92a1710c34195fdd9c949f7ebb5a0e7d0167
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-06-09 21:15:04 +0530

    only reset api remains

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'6\xe9\xda\xead\x81\xf7\x8d\xbbH\x87\xe8m\xdd3%'
+
+    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment' }
+
+
     
-    MONGODB_SETTINGS= { 'db' : 'UTA_Enrollment'}
